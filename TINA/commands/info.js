16db002f41f46bfa4a99ef7f588e70e32eeb9560
SHABBIR_HUSAIN_munna <shabbirrohoman24@gmail.com>
2025-02-23 01:10:00 +0600

    Update info.js

--- TINA/commands/info.js
+++ TINA/commands/info.js
@@ -24,33 +24,33 @@ const time = process.uptime(),
 		seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-var link = ["https://i.postimg.cc/3JpSf0LC/received-1709835639483729.jpg", 
+var link = ["https://i.postimg.cc/fLqQw7W0/IMG-1740150858879.jpg", 
             
-            "https://i.postimg.cc/28zrCYPW/received-2253672584826375.jpg", 
+            "https://i.postimg.cc/Zqj1cPdD/Riyan-20241227-160846-Portrait-Killer-by-Riyan.jpg", 
             
-            "https://i.postimg.cc/3JpSf0LC/received-1709835639483729.jpg",
+            "https://i.postimg.cc/fLqQw7W0/IMG-1740150858879.jpg",
 
-"https://i.postimg.cc/3JpSf0LC/received-1709835639483729.jpg",
+"https://i.postimg.cc/Zqj1cPdD/Riyan-20241227-160846-Portrait-Killer-by-Riyan.jpg",
             
-            "https://i.postimg.cc/28zrCYPW/received-2253672584826375.jpg"];
+            "https://i.postimg.cc/256fgXrf/FB-IMG-1733586252317.jpg"];
   
-var callback = () => api.sendMessage({body:` 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡
+var callback = () => api.sendMessage({body:` 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡✨
 ======================
 𝗕𝗢𝗧 𝗡𝗔𝗠𝗘 : ${global.config.BOTNAME}
-𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 : 𝗡𝗔𝗭𝗥𝗨𝗟
+𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 : 𝗦𝗛𝗔𝗕𝗕𝗜𝗥
 𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝗡𝗔𝗢𝗚𝗔𝗢𝗡
 ======𝗖𝗢𝗡𝗧𝗔𝗖𝗧======
 ======================
-𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com
 𝗠𝗔𝗦𝗦𝗘𝗡𝗚𝗘𝗥 𝗖𝗛𝗔𝗧 𝗟𝗜𝗡𝗞:
-https://m.me/ji.la.pi.6
+https://m.me
 𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫: ${global.config.PREFIX}
-𝗕𝗢𝗧 𝗪𝗢𝗡𝗘𝗥: 𝗡𝗔𝗭𝗥𝗨𝗟
+𝗕𝗢𝗧 𝗪𝗢𝗡𝗘𝗥: 𝗦𝗛𝗔𝗕𝗕𝗜𝗥
 𝗢𝗧𝗛𝗘𝗥 𝗡𝗘𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡 ============================================
 𝗧𝗢𝗗𝗬 𝗜𝗦 𝗧𝗜𝗠𝗘 : ${juswa} 
 𝗕𝗢𝗧 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚 ${hours}:${minutes}:${seconds}.
 𝗧𝗛𝗔𝗡𝗞𝗦 𝗙𝗢𝗥 𝗨𝗦𝗜𝗡𝗚 ${global.config.BOTNAME} \n======================
 ✢━━━━━ N/S ━━━━━✢
-----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖---- `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+----❖----- 𝘀⃪𝗵⃪𝗮⃪𝗯⃪𝗯⃪𝗶⃪𝗿⃪ -----❖---- `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
    };
